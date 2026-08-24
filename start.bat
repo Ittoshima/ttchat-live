50ffe2067fcb5f2f2627c875c8697b4f87ec5a0f
MRAB <161315698+Ittoshima@users.noreply.github.com>
2026-08-24 15:20:47 +0700

    Deploy: Add start.bat

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+title TTChat Pro - TikTok Live Chat Reader
+echo ====================================================
+echo        🚀 TTChat Pro - TikTok Live Suite
+echo ====================================================
+echo.
+echo Starting server at http://localhost:3000 ...
+start http://localhost:3000
+node server.js
+pause
